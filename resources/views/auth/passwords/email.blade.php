61f8ac61a76e3f40dcf78b002e00a7a39003ddbf
Vladislav <vyacheslav.dimitrov@gmail.com>
2017-11-13 08:44:55 +0300

    Fixed reset password screen.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,54 @@
-@extends('layouts.app')
+@extends('layouts.start')
 
 @section('content')
+
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
+<div class="row m-b-40">
+    <div class="col-md-4">
+	&nbsp;
+    </div>
+    <div class="form-container col-md-4">
+        <div class="block">
+            <div class="row">
+		<div class="col-xs-12 active-header-label">
+		    Восстановление пароля
+		</div>
+	    </div>
+	    <hr />
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <p class="m-b-20 label-email">Введите E-mail, указанный при регистрации</p>
+            <form name="form" novalidate  method="POST" action="{{ route('password.email') }}">
+                {{ csrf_field() }}
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="form-group floating-labels">
+                            <label for="email-1">Почта</label>
+                            <input id="email-1" autocomplete="off" type="email" name="email">
+			    <p class="error-block">
+			    </p>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        {{ $errors->first('email') }}
                                     </span>
                                 @endif
-                            </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
+                <div class="row">
+                    <div class="col-xs-5">
+                        <input type="submit" class="btn btn-xs btn-ol-login" value="Восстановить">
+                    </div>
+                    <div class="col-xs-4">
+                        <a class="btn-ol-cancel" href="/">Отмена</a>
+		    </div>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 </div>
 @endsection
