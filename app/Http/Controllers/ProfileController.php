f57835a9723ca77a1a2a906ab34e19858c764813
Vladislav <vyacheslav.dimitrov@gmail.com>
2017-11-16 08:32:37 +0300

    Added saving profile.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\User;
 use Auth;
+use Log;
 
 class ProfileController extends Controller
 {
@@ -41,13 +43,24 @@ class ProfileController extends Controller
     }
     
     /**
-     * Show the user profile for editing.
+     * Save user profile to database.
      *
      * @return \Illuminate\Http\Response
      */
-    public function profileSave()
+    public function profileSave(Request $request)
     {
-        return view('profile');
+	$oUser = Auth::user();
+	Log::info('from save profile');
+	$aFields = array('profile_first_name', 'profile_second_name', 'profile_middle_name',
+		'profile_about', 'profile_height', 'profile_marital_status', 'profile_children', 
+		'profile_location', 'profile_location_birth', 'gender', 'birthday');
+	foreach($aFields as $sField) {
+	    if (isset($request->{$sField}) && !empty($request->{$sField})) {
+		$oUser->{$sField} = $request->{$sField};
+	    }
+	}
+	$oUser->Save();
+	return response()->json(array('result' => 'ok'));
     }
 }
 
--- resources/assets/js/ol.js
+++ resources/assets/js/ol.js
@@ -11,15 +11,24 @@ ol.manager = function () {
          */
         this.saveProfile = function() {
             var self = this;
-	    console.log('from save profile');
 	    $("#save_profile_btn").button('loading');
-            $.post('/profile/save', $('#user-profile-form'), function(result) {
-                $("#save_profile_btn").button('reset');
-		if (result.error) {
-                    return;
-                }
+	    console.log('from save profile:' + $('meta[name="csrf-token"]').attr('content'));
+            $.ajax({
+		url: '/profile/save', 
+		type: 'post',
+		data: $('#user-profile-form').serialize(),
+		headers: {
+		    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+		},	
+		success: function(result) {
+		    console.log('after save profile');
+		    $("#save_profile_btn").button('reset');
+		    if (result.error) {
+			return;
+		    }
 		
-            }.bind(this));
+		},
+	    });
             return false;
         };
 
