c033d7705c4714388d133daa2c75623fcd17a061
Vladislav <vyacheslav.dimitrov@gmail.com>
2017-12-05 13:47:55 +0300

    Added link to panel from list of panels.

--- resources/views/admin/panels_table.blade.php
+++ resources/views/admin/panels_table.blade.php
@@ -9,7 +9,7 @@
     <tbody>
 	@foreach($panels as $panel)
 	<tr>
-	    <td>{{$panel->name}}</td>
+	    <td><a href="/panel/{{$panel->id}}">{{$panel->name}}</a></td>
 	    <td>{{count($panel->getMarkers())}}</td>
 	    <td>
 		<a href="/admin/panel/edit/{{$panel->id}}" class="edit_panel" data-marker-id="{{$panel->id}}"><span class="glyphicon glyphicon-edit"></span></a>
